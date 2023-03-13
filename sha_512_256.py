2c54768b5d83a1cbb3d6f83daefa43c6bd364c7a
dBaliukynas <domas.baliukynas@gmail.com>
2023-03-13 17:49:54 +0200

    Update padding zeroes length algorithm

--- sha_512_256.py
+++ sha_512_256.py
@@ -4,8 +4,6 @@ initial_sha_512_256_hash_values = [0x022312194fc2bf72c, 0x9f555fa3c84c64c2, 0x23
 
 def split_into_chunks(input):
   chunks = []
-  input_length = len(input) * 8
-  chunk_count = input_length // 896 + 1
 
   pre_process(input)
 
@@ -13,16 +11,26 @@ def split_into_chunks(input):
 def pre_process(input):
     message_block = input
     input_length = len(input) * 8
+    print(input_length)
+
     message_block += ((1 << 7).to_bytes(1, byteorder='little'))
-    message_block.extend(bytearray((896 - (input_length % 896) - 8) // 8))
+    padding_zeroes_length = (895 - input_length) % 1024 // 8
+    message_block.extend(bytearray(padding_zeroes_length))
     message_block += (input_length).to_bytes(16, byteorder='big')
-    for (i, char) in enumerate(message_block):
-      if ((i + 1) % 4 == 0):
-        print(format(char, '08b'))
-      else:
-        print(format(char, '08b'), end=' ')
-    # print(message_block)
+
+    print(len(message_block))
+
+    print_message_block(message_block)
+
     return message_block
+
+def print_message_block(message_block):
+  for (i, char) in enumerate(message_block):
+    if ((i + 1) % 4 == 0):
+      print(format(char, '08b'))
+    else:
+      print(format(char, '08b'), end=' ')
+
 
 # split_into_chunks(input)
 
